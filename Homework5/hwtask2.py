de505ba7b3e69aa671f4b57d500fcb60523d15c7
Ludmila <ludmila0704@yandex.ru>
2023-07-04 22:51:05 +0300

    HW5

--- Homework5/hwtask2.py
+++ Homework5/hwtask2.py
@@ -3,7 +3,7 @@ names = ['Борис', 'Маргарита', 'Руслан', 'Дариман']
 premiums = [5000, 9500, 4000, 15000]  # ставка
 rates = ['10.25%', '15.2%', '6.8%', '18.96%']  # премия в %
 
-text_dict = {name: premium * float(rate[:-1]) for (name, premium, rate) in zip(names, premiums, rates)}
+text_dict = {name: premium * float(rate[:-1])/100 for (name, premium, rate) in zip(names, premiums, rates)}
 
 if __name__ == "__main__":
     print(text_dict)
